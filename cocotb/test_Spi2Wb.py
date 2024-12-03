b6b27b967063704fcc2cfd64d08b8dd5b4179833
Kevin Joly <kevin.joly@armadeus.com>
2024-12-03 14:33:28 +0100

    Fix testbench

--- cocotb/test_Spi2Wb.py
+++ cocotb/test_Spi2Wb.py
@@ -29,7 +29,6 @@ except KeyError:
 
 
 class TestSpi2Wb(object):
-    INTERFRAME = (100, "ns")
 
     def __init__(self, dut, clock, cpol=0, cpha=1,
                  datasize=DATASIZE, addr_ext=EXTADDR,
@@ -55,147 +54,93 @@ class TestSpi2Wb(object):
             raise NotImplementedError("cpol = 1 not implemented yet")
         if cpha == 0:
             raise NotImplementedError("cpha = 0 not implemented yet")
-        self._clock_thread = cocotb.fork(clock.start())
 
-        spi_sigs = SPISignals(miso=dut.miso,
-                              mosi=dut.mosi,
-                              sclk=dut.sclk,
-                              cs=dut.csn)
+        self._clock_thread = cocotb.start_soon(clock.start())
 
-        self.spi_config = SPIConfig(cpol=False,
-                                    cpha=True,
-                                    baudrate=(1, "us"),
-                                    csphase=False)
+        spi_bus = SpiBus.from_entity(dut, cs_name='csn')
 
-        self.spimod = SPIModule(self.spi_config, spi_sigs, clock)
+        self.spi_config = SpiConfig(word_width = int(datasize),
+                                    sclk_freq = 1e6,
+                                    cpol = False,
+                                    cpha = True,
+                                    msb_first = True,
+                                    data_output_idle = 1,
+                                    frame_spacing_ns = 100,
+                                    ignore_rx_value = None,
+                                    cs_active_low = True)
+
+        self.spimod = SpiMaster(spi_bus, self.spi_config)
 
     async def reset(self):
-        self._dut.rstn <= 0
+        self._dut.rstn.value = 0
         short_per = Timer(100, units="ns")
-        self._dut.rstn <= 0
-        self.spimod.set_cs(False)
-        self._dut.mosi <= 0
-        self._dut.sclk <= 0
         await short_per
-        self._dut.rstn <= 1
+        self._dut.rstn.value = 1
         await short_per
+
+    async def _transfer(self, addr, values=[], datasize=8, write=False, burst=False):
+        if self.addr_ext:
+            write_bit = 0x8000
+            burst_bit = 0x4000
+            addr = addr & 0xffff
+        else:
+            write_bit = 0x80
+            burst_bit = 0x40
+            addr = addr & 0xff
+
+        b = []
+
+        if write:
+            addr = addr | write_bit
+
+        if burst:
+            addr = addr | burst_bit
+
+        b.append(addr)
+
+        for v in values:
+            if datasize == 16:
+                b.append(v & 0xffff)
+            else:
+                b.append(v & 0xff)
+
+        await self.spimod.write(b, burst = True)
+        return await self.spimod.read()
 
     async def write_byte(self, addr, value, datasize=8):
         if not datasize in [8, 16]:
             raise NotImplementedError("Size {} not supported".format(datasize))
-        sclk_per = Timer(self.spi_config.baudrate[0],
-                         units=self.spi_config.baudrate[1])
-        self.spimod.set_cs(True)
-        await sclk_per
-        if not self.addr_ext:
-            await self.spimod.send(0x80|addr)
-        else:
-            await self.spimod.send(0x80|((addr >> 8)&0xFF))
-            await self.spimod.send(addr&0xFF)
-        await Timer(self.INTERFRAME[0], units=self.INTERFRAME[1])
-        if datasize == 16:
-            await self.spimod.send((value >> 8)&0x00FF)
-        await self.spimod.send(value&0x00FF)
-        await sclk_per
-        self.spimod.set_cs(False)
-        await sclk_per
- 
+
+        return await self._transfer(addr, [value], datasize, write=True, burst=False)
+
     async def read_byte(self, addr, datasize=8):
        if not datasize in [8, 16]:
             raise NotImplementedError("Size {} not supported".format(datasize))
-       sclk_per = Timer(self.spi_config.baudrate[0],
-                        units=self.spi_config.baudrate[1])
-       self.spimod.set_cs(True)
-       await sclk_per
-       if not self.addr_ext:
-           await self.spimod.send(0x7F&addr)
-       else:
-           await self.spimod.send((addr>>8)&0x7F)
-           await self.spimod.send(addr&0xFF)
-       await sclk_per
-       await self.spimod.send(0x00)  # let _monitor_recv getting value
-       if datasize == 16:
-           await self.spimod.send(0x00)
-       await sclk_per
-       self.spimod.set_cs(False)
-       ret = await self.spimod.wait_for_recv(1) # waiting for receive value
-       await sclk_per
-       try:
-           value_read = int(ret["miso"][-datasize:], 2)
-       except ValueError:
-           value_read = ret["miso"][-datasize:]
-
-       return value_read
+       return await self._transfer(addr, [0x0], datasize, write=False, burst=False)
 
     async def burst_write(self, addr, lvalues=[], datasize=8):
-        if BURST != "1":
+        if BURST != "Burst":
             raise NotImplementedError("BURST synthesis option not set")
         if not datasize in [8, 16]:
             raise NotImplementedError("Size {} not supported".format(datasize))
         if len(lvalues) < 2:
             raise NotImplementedError("should be at least 2 bytes lenght burst")
-        sclk_per = Timer(self.spi_config.baudrate[0],
-                         units=self.spi_config.baudrate[1])
 
-        self.spimod.set_cs(True)
-        # sending address
-        await sclk_per
-        if not self.addr_ext:
-            await self.spimod.send((0x3F&addr)|0xC0)
-        else:
-            await self.spimod.send(((addr>>8)&0x3F)|0xC0)
-            await self.spimod.send(addr&0xFF)
-
-        await sclk_per
-        for value in lvalues:
-            if datasize == 16:
-                await self.spimod.send((value>>8)&0x00FF)
-            await self.spimod.send(value&0x00FF)
-            await sclk_per
-
-        self.spimod.set_cs(False)
-        await sclk_per
+        return await self._transfer(addr, lvalues, datasize, write=True, burst=True)
 
     async def burst_read(self, addr, datasize=8, nbByte=10):
-        if BURST != "1":
+        if BURST != "Burst":
             raise NotImplementedError("BURST synthesis option not set")
         if not datasize in [8, 16]:
             raise NotImplementedError("Size {} not supported".format(datasize))
         if nbByte < 2:
             raise NotImplementedError("should be at least 2 bytes lenght burst")
-        sclk_per = Timer(self.spi_config.baudrate[0],
-                         units=self.spi_config.baudrate[1])
-        
-        self.spimod.set_cs(True)
-        ret_values = []
 
-        # sending address
-        await sclk_per
-        if not self.addr_ext:
-            await self.spimod.send((0x3F&addr)|0x40)
-        else:
-            await self.spimod.send(((addr>>8)&0x3F)|0x40)
-            await self.spimod.send(addr&0xFF)
-        await sclk_per
-        for vbyte in range(nbByte):
-            await self.spimod.send(0x00)  # let _monitor_recv getting value
-            if datasize == 16:
-                await self.spimod.send(0x00)
-            await sclk_per
+        b = []
+        for _ in range(nbByte):
+            b.append(0x0)
 
-        self.spimod.set_cs(False)
-        ret = await self.spimod.wait_for_recv(1) # waiting for receive value
-        await sclk_per
-        print(f"debug {ret}")
-        try:
-            value_read = int(ret["miso"][-datasize:], 2)
-        except ValueError:
-            value_read = ret["miso"][-datasize:]
-        for n in range(nbByte):
-            ret_values.append(ret["miso"][(-datasize*(n+1)):(len(ret["miso"])-datasize*n)])
-
-        ret_values.reverse()
-        return ret_values
+        return await self._transfer(addr, b, datasize, write=False)
 
 @cocotb.test(skip=not Tburst_read)
 async def test_burst_read(dut):
@@ -221,16 +166,18 @@ async def test_burst_read(dut):
     await Timer(50, units="us")
 
     # read burst
-    readret = await tspi2wb.burst_read(0x10, datasize=16, nbByte=5)
+    readret = await tspi2wb.burst_read(0x10, datasize=16, nbByte=6)
 
     dut._log.info("DEBUG")
     dut._log.info(readret)
 
-    for value in readret:
-        try:
-            print(f" {value} -> 0x{hex(int(value, 2))}")
-        except ValueError:
-            print(f" {value} -> Unknown value")
+    for readval, expval in zip(readret[1:], writevalues):
+        if readval != expval:
+            msg = ("Value read 0x{:x} @0x{:02X} should be 0x{:02X}"
+                    .format(readval, addr, expval))
+            dut._log.error(msg)
+            test_msg.append(msg)
+            test_success = False
 
     if not test_success:
         raise TestFailure("\n".join(test_msg))
@@ -241,11 +188,13 @@ async def test_one_data_frame(dut):
     test_msg = []
     dut._log.info("Launching tspi2wb test")
     tspi2wb = TestSpi2Wb(dut, Clock(dut.clock, 1, "ns"))
-    datasize = int(tspi2wb.datasize)
+    await Timer(10, 'us')
     if tspi2wb.addr_ext:
         dut._log.info("Address is extended to 15bits")
+    datasize = int(tspi2wb.datasize)
     dut._log.info("Datasize is {}".format(datasize))
     await tspi2wb.reset()
+
     sclk_per = Timer(10, units="ns")
     short_per = Timer(100, units="ns")
     if datasize == 8:
@@ -268,7 +217,9 @@ async def test_one_data_frame(dut):
 
     # Reading back
     for addr, value in testvalues:
-        vread = await tspi2wb.read_byte(addr, datasize=datasize)
+        val = await tspi2wb.read_byte(addr)
+        vread = val[1]
+
         dut._log.info("Read byte 0x{:x} @ 0x{:02X} (should be 0x{:02X})"
                 .format(vread, addr, value))
         if int(vread) != value:
